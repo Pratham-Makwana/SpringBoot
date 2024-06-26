297ef2e4a52af0db24cfd117e189108d96e79784
Pratham-Makwana <makwanapratham13@gmail.com>
2024-06-26 10:22:18 +0530

    Integrate MongoDB in the Spring Application

--- jounralApp/src/main/java/com/pratham/journalApp/controller/JournalEntryController.java
+++ jounralApp/src/main/java/com/pratham/journalApp/controller/JournalEntryController.java
@@ -1,11 +1,57 @@
 package com.pratham.journalApp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.pratham.journalApp.entity.JournalEntry;
+import com.pratham.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/journal")
 public class JournalEntryController {
+
+    @Autowired
+    private JournalEntryService journalEntryService;
+
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return journalEntryService.getAll();
+    }
+
+    @PostMapping
+    public JournalEntry createEntry(@RequestBody JournalEntry myEntry){
+        myEntry.setDate(LocalDateTime.now());
+        journalEntryService.saveEntry(myEntry);
+        return myEntry;
+    }
+
+    @GetMapping("id/{id}")
+    public JournalEntry getJournalEntriesById(@PathVariable ObjectId id){
+        return journalEntryService.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("id/{id}")
+    public Boolean deleteJournalEntriesById(@PathVariable ObjectId id){
+         journalEntryService.deleteById(id);
+         return true;
+    }
+
+    @PutMapping("id/{id}")
+    public JournalEntry updateJournalEntriesById(@PathVariable ObjectId id,@RequestBody JournalEntry newEntry){
+            JournalEntry oldEntry = journalEntryService.findById(id).orElse(null);
+            if (oldEntry != null){
+                oldEntry.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : oldEntry.getTitle());
+                oldEntry.setContent(newEntry.getContent() != null && !newEntry.getTitle().equals("") ? newEntry.getContent() : oldEntry.getContent());
+            }
+
+            journalEntryService.saveEntry(oldEntry);
+            return oldEntry;
+    }
+
 
 
 }
--- jounralApp/src/main/java/com/pratham/journalApp/repository/JournalEntryRepository.java
+++ jounralApp/src/main/java/com/pratham/journalApp/repository/JournalEntryRepository.java
@@ -1,9 +1,10 @@
 package com.pratham.journalApp.repository;
 
 import com.pratham.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface JournalEntryRepository extends MongoRepository<JournalEntry,String> {
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
 }
 
 // MongoRepository<JournalEntry,String> first parameter where you what to perform operation and second type of id
