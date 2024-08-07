c23062fbc69dabd80e8b4110dcc0eeebd81439b6
Pratham-Makwana <makwanapratham13@gmail.com>
2024-08-07 17:05:37 +0530

    Role Based Authentication

--- securityDemo/src/main/java/com/pratham/securityDemo/Greeting.java
+++ securityDemo/src/main/java/com/pratham/securityDemo/Greeting.java
@@ -1,5 +1,6 @@
 package com.pratham.securityDemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,17 @@ public class Greeting {
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndPoint(){
+        return "Hello User!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndPoint(){
+        return "Hello Admin!";
     }
 }
